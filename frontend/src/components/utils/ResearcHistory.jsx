06437f02348806b07c14708e60e410fa7cabd299
Tslil Aharon <59604062+tslilaharon@users.noreply.github.com>
2025-04-29 15:54:27 +0300

    Fix import paths and graph metrics usage in ResearchHistory

--- frontend/src/components/utils/ResearcHistory.jsx
+++ frontend/src/components/utils/ResearcHistory.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp, FaWikipediaW } from 'react-icons/fa';
 import { Row, Col, Button, Card, Table, Form } from 'react-bootstrap';
 import { ChevronLeft, ChevronRight, FileBarGraph } from 'react-bootstrap-icons';
 import MetricsButton from '../common/MetricsButton';
-import { graphMetrics } from '../../pages/Home';
+import { graphMetrics } from "../../constants/graphMetrics";
 import { toast } from 'react-hot-toast';
 import { useState, useEffect, useRef } from 'react';
 import MetricsPanel from '../network/MetricsPanel';
@@ -12,8 +12,6 @@ import NetworkCustomizationToolbar from '../NetworkCustomizationToolbar';
 import NetworkGraph from '../network/NetworkGraph';
 import NetworkDataTable from '../NetworkDataTable';
 import { GraphContainer } from './StyledComponents-El';
-
-
 
 
 const ResearchHistory = ({ research }) => {
