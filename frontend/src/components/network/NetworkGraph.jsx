3432179a4a02c5e209ab02fab79ceff9afd32923
Tslil Aharon <59604062+tslilaharon@users.noreply.github.com>
2025-06-04 13:52:07 +0300

    chore: enhance network graph rendering and update styles

--- frontend/src/components/network/NetworkGraph.jsx
+++ frontend/src/components/network/NetworkGraph.jsx
@@ -4,17 +4,17 @@ import PropTypes from "prop-types";
 
 const NetworkGraph = ({
   networkData,
-  filteredNodes,
-  filteredLinks,
+  filteredNodes = [],
+  filteredLinks = [],
   customizedNetworkData,
   selectedMetric,
-  highlightCentralNodes,
-  showMetrics,
-  visualizationSettings,
+  highlightCentralNodes = false,
+  showMetrics = true,
+  visualizationSettings = {},
   handleNodeClick,
-  networkWasRestored,
+  networkWasRestored = false,
   forceGraphRef,
-  isDirectedGraph,
+  isDirectedGraph = false,
 }) => {
   useEffect(() => {
     if (forceGraphRef.current) {
@@ -27,254 +27,257 @@ const NetworkGraph = ({
   }
 
   return (
-    <div className="graph-wrapper"  style={{
-      maxWidth: "100%",
-      width: "100%",
-      height: "700px",
-      overflowX: "hidden",
-      overflowY: "hidden",
-      position: "relative",
-      margin: "0 auto",
-    }}>
-    <ForceGraph2D
-      ref={forceGraphRef}
-      key={customizedNetworkData ? "customized" : "default"}
-      graphData={{
-        nodes: customizedNetworkData
-          ? customizedNetworkData.nodes
-          : filteredNodes,
-        links: customizedNetworkData
-          ? customizedNetworkData.links.map((link) => {
-              const sourceNode = customizedNetworkData.nodes.find(
-                (node) =>
-                  (typeof link.source === "object" &&
-                    node.id === link.source.id) ||
-                  node.id === link.source
-              );
-
-              const targetNode = customizedNetworkData.nodes.find(
-                (node) =>
-                  (typeof link.target === "object" &&
-                    node.id === link.target.id) ||
-                  node.id === link.target
-              );
-
-              return {
-                source: sourceNode || link.source,
-                target: targetNode || link.target,
-                weight: link.weight || 1,
-              };
-            })
-          : filteredLinks.map((link) => {
-              const sourceNode = filteredNodes.find(
-                (node) =>
-                  (typeof link.source === "object" &&
-                    node.id === link.source.id) ||
-                  node.id === link.source
-              );
-
-              const targetNode = filteredNodes.find(
-                (node) =>
-                  (typeof link.target === "object" &&
-                    node.id === link.target.id) ||
-                  node.id === link.target
-              );
-
-              return {
-                source: sourceNode || link.source,
-                target: targetNode || link.target,
-                weight: link.weight || 1,
-              };
-            }),
+    <div
+      className="graph-wrapper"
+      style={{
+        maxWidth: "100%",
+        width: "100%",
+        height: "700px",
+        overflowX: "hidden",
+        overflowY: "hidden",
+        position: "relative",
+        margin: "0 auto",
       }}
-      directed={isDirectedGraph}
-      width={showMetrics ? 1200 : 1500}
-      height={700}
-      fitView
-      fitViewPadding={20}
-      nodeAutoColorBy={customizedNetworkData ? null : "id"}
-      linkWidth={(link) => Math.sqrt(link.weight || 1)}
-      linkColor={() => "gray"}
-      enableNodeDrag={true}
-      cooldownTicks={100}
-      d3AlphaDecay={0.03}
-      d3VelocityDecay={0.2}
-      onNodeClick={handleNodeClick}
-      onEngineStop={() => forceGraphRef.current?.zoomToFit(400, 100)}
-      onNodeDragEnd={(node) => {
-        node.fx = node.x;
-        node.fy = node.y;
-      }}
-      linkCanvasObject={(link, ctx, globalScale) => {
-        if (!link.source || !link.target) return;
-        ctx.beginPath();
-        ctx.moveTo(link.source.x, link.source.y);
-        ctx.lineTo(link.target.x, link.target.y);
-        ctx.strokeStyle = "gray";
-        ctx.lineWidth = Math.sqrt(link.weight || 1);
-        ctx.stroke();
-        const midX = (link.source.x + link.target.x) / 2;
-        const midY = (link.source.y + link.target.y) / 2;
-        const fontSize = 10 / globalScale;
-        ctx.save();
-        ctx.font = `${fontSize}px Sans-Serif`;
-        ctx.fillStyle = "black";
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.fillText(link.weight || "1", midX, midY);
-        ctx.restore();
+    >
+      <ForceGraph2D
+        ref={forceGraphRef}
+        key={customizedNetworkData ? "customized" : "default"}
+        graphData={{
+          nodes: customizedNetworkData
+            ? customizedNetworkData.nodes
+            : filteredNodes,
+          links: customizedNetworkData
+            ? customizedNetworkData.links.map((link) => {
+                const sourceNode = customizedNetworkData.nodes.find(
+                  (node) =>
+                    (typeof link.source === "object" &&
+                      node.id === link.source.id) ||
+                    node.id === link.source
+                );
 
-        if (isDirectedGraph) {
-          const targetRadius =
-            link.target.size ||
+                const targetNode = customizedNetworkData.nodes.find(
+                  (node) =>
+                    (typeof link.target === "object" &&
+                      node.id === link.target.id) ||
+                    node.id === link.target
+                );
+
+                return {
+                  source: sourceNode || link.source,
+                  target: targetNode || link.target,
+                  weight: link.weight || 1,
+                };
+              })
+            : filteredLinks.map((link) => {
+                const sourceNode = filteredNodes.find(
+                  (node) =>
+                    (typeof link.source === "object" &&
+                      node.id === link.source.id) ||
+                    node.id === link.source
+                );
+
+                const targetNode = filteredNodes.find(
+                  (node) =>
+                    (typeof link.target === "object" &&
+                      node.id === link.target.id) ||
+                    node.id === link.target
+                );
+
+                return {
+                  source: sourceNode || link.source,
+                  target: targetNode || link.target,
+                  weight: link.weight || 1,
+                };
+              }),
+        }}
+        directed={isDirectedGraph}
+        width={showMetrics ? 1200 : 1500}
+        height={700}
+        fitView
+        fitViewPadding={20}
+        nodeAutoColorBy={customizedNetworkData ? null : "id"}
+        linkWidth={(link) => Math.sqrt(link.weight || 1)}
+        linkColor={() => "gray"}
+        enableNodeDrag={true}
+        cooldownTicks={100}
+        d3AlphaDecay={0.03}
+        d3VelocityDecay={0.2}
+        onNodeClick={handleNodeClick}
+        onEngineStop={() => forceGraphRef.current?.zoomToFit(400, 100)}
+        onNodeDragEnd={(node) => {
+          node.fx = node.x;
+          node.fy = node.y;
+        }}
+        linkCanvasObject={(link, ctx, globalScale) => {
+          if (!link.source || !link.target) return;
+          ctx.beginPath();
+          ctx.moveTo(link.source.x, link.source.y);
+          ctx.lineTo(link.target.x, link.target.y);
+          ctx.strokeStyle = "gray";
+          ctx.lineWidth = Math.sqrt(link.weight || 1);
+          ctx.stroke();
+          const midX = (link.source.x + link.target.x) / 2;
+          const midY = (link.source.y + link.target.y) / 2;
+          const fontSize = 10 / globalScale;
+          ctx.save();
+          ctx.font = `${fontSize}px Sans-Serif`;
+          ctx.fillStyle = "black";
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle";
+          ctx.fillText(link.weight || "1", midX, midY);
+          ctx.restore();
+
+          if (isDirectedGraph) {
+            const targetRadius =
+              link.target.size ||
+              (selectedMetric === "PageRank Centrality"
+                ? Math.max(10, link.target.pagerank * 500)
+                : selectedMetric === "Eigenvector Centrality"
+                ? Math.max(10, link.target.eigenvector * 60)
+                : selectedMetric === "Closeness Centrality"
+                ? Math.max(10, link.target.closeness * 50)
+                : selectedMetric === "Betweenness Centrality"
+                ? Math.max(10, link.target.betweenness * 80)
+                : selectedMetric === "Degree Centrality"
+                ? Math.max(10, link.target.degree * 80)
+                : 20);
+
+            const dx = link.target.x - link.source.x;
+            const dy = link.target.y - link.source.y;
+            const dist = Math.sqrt(dx * dx + dy * dy);
+
+            if (dist === 0) return;
+
+            const arrowX = link.target.x - (dx * targetRadius) / dist;
+            const arrowY = link.target.y - (dy * targetRadius) / dist;
+
+            const angle = Math.atan2(dy, dx);
+            const arrowLength = 10 / globalScale;
+            const arrowWidth = 5 / globalScale;
+
+            ctx.save();
+            ctx.translate(arrowX, arrowY);
+            ctx.rotate(angle);
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(-arrowLength, arrowWidth);
+            ctx.lineTo(-arrowLength, -arrowWidth);
+            ctx.closePath();
+            ctx.fillStyle = "gray";
+            ctx.fill();
+            ctx.restore();
+          }
+        }}
+        nodeCanvasObject={(node, ctx, globalScale) => {
+          const fontSize = 12 / globalScale;
+
+          const radius =
+            node.size ||
             (selectedMetric === "PageRank Centrality"
-              ? Math.max(10, link.target.pagerank * 500)
+              ? Math.max(10, node.pagerank * 500)
               : selectedMetric === "Eigenvector Centrality"
-              ? Math.max(10, link.target.eigenvector * 60)
+              ? Math.max(10, node.eigenvector * 60)
               : selectedMetric === "Closeness Centrality"
-              ? Math.max(10, link.target.closeness * 50)
+              ? Math.max(10, node.closeness * 50)
               : selectedMetric === "Betweenness Centrality"
-              ? Math.max(10, link.target.betweenness * 80)
+              ? Math.max(10, node.betweenness * 80)
               : selectedMetric === "Degree Centrality"
-              ? Math.max(10, link.target.degree * 80)
+              ? Math.max(10, node.degree * 80)
               : 20);
 
-          const dx = link.target.x - link.source.x;
-          const dy = link.target.y - link.source.y;
-          const dist = Math.sqrt(dx * dx + dy * dy);
-
-          if (dist === 0) return;
-
-          const arrowX = link.target.x - (dx * targetRadius) / dist;
-          const arrowY = link.target.y - (dy * targetRadius) / dist;
-
-          const angle = Math.atan2(dy, dx);
-          const arrowLength = 10 / globalScale;
-          const arrowWidth = 5 / globalScale;
-
           ctx.save();
-          ctx.translate(arrowX, arrowY);
-          ctx.rotate(angle);
           ctx.beginPath();
-          ctx.moveTo(0, 0);
-          ctx.lineTo(-arrowLength, arrowWidth);
-          ctx.lineTo(-arrowLength, -arrowWidth);
-          ctx.closePath();
-          ctx.fillStyle = "gray";
+          ctx.arc(node.x, node.y, radius, 0, 2 * Math.PI, false);
+
+          const isHighlighted = node.highlighted && highlightCentralNodes;
+
+          const nodeColor = isHighlighted
+            ? "#ff9900"
+            : node.color ||
+              (selectedMetric === "PageRank Centrality"
+                ? "orange"
+                : selectedMetric === "Eigenvector Centrality"
+                ? "purple"
+                : selectedMetric === "Closeness Centrality"
+                ? "green"
+                : selectedMetric === "Betweenness Centrality"
+                ? "red"
+                : selectedMetric === "Degree Centrality"
+                ? "#231d81"
+                : "blue");
+
+          ctx.fillStyle = nodeColor;
           ctx.fill();
+
+          if (node.isHighlightedCommunity) {
+            ctx.save();
+            ctx.shadowColor = "#F5BD20";
+            ctx.shadowBlur = 15;
+            ctx.shadowOffsetX = 0;
+            ctx.shadowOffsetY = 0;
+            ctx.strokeStyle = "#F5BD20";
+            ctx.lineWidth = 2;
+            ctx.stroke();
+            ctx.restore();
+          } else if (isHighlighted) {
+            ctx.strokeStyle = "#ffff00";
+            ctx.lineWidth = 3;
+            ctx.stroke();
+          }
+
+          ctx.font = `${fontSize}px Sans-Serif`;
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle";
+          ctx.fillStyle = "white";
+          ctx.fillText(node.id, node.x, node.y);
+
+          if (selectedMetric === "Degree Centrality") {
+            ctx.fillStyle = "#231d81";
+            ctx.fillText(`Deg: ${node.degree}`, node.x, node.y + radius + 5);
+          }
+          if (selectedMetric === "Betweenness Centrality") {
+            ctx.fillStyle = "DarkRed";
+            ctx.fillText(
+              `Btw: ${node.betweenness?.toFixed(2) || 0}`,
+              node.x,
+              node.y + radius + 5
+            );
+          }
+          if (selectedMetric === "Closeness Centrality") {
+            ctx.fillStyle = "green";
+            ctx.fillText(
+              `Cls: ${node.closeness?.toFixed(2) || 0}`,
+              node.x,
+              node.y + radius + 5
+            );
+          }
+          if (selectedMetric === "Eigenvector Centrality") {
+            ctx.fillStyle = "purple";
+            ctx.fillText(
+              `Eig: ${node.eigenvector?.toFixed(4) || 0}`,
+              node.x,
+              node.y + radius + 5
+            );
+          }
+          if (selectedMetric === "PageRank Centrality") {
+            ctx.fillStyle = "orange";
+            ctx.fillText(
+              `PR: ${node.pagerank?.toFixed(4) || 0}`,
+              node.x,
+              node.y + radius + 5
+            );
+          }
+
           ctx.restore();
-        }
-      }}
-      nodeCanvasObject={(node, ctx, globalScale) => {
-        const fontSize = 12 / globalScale;
-
-        const radius =
-          node.size ||
-          (selectedMetric === "PageRank Centrality"
-            ? Math.max(10, node.pagerank * 500)
-            : selectedMetric === "Eigenvector Centrality"
-            ? Math.max(10, node.eigenvector * 60)
-            : selectedMetric === "Closeness Centrality"
-            ? Math.max(10, node.closeness * 50)
-            : selectedMetric === "Betweenness Centrality"
-            ? Math.max(10, node.betweenness * 80)
-            : selectedMetric === "Degree Centrality"
-            ? Math.max(10, node.degree * 80)
-            : 20);
-
-        ctx.save();
-        ctx.beginPath();
-        ctx.arc(node.x, node.y, radius, 0, 2 * Math.PI, false);
-
-        const isHighlighted = node.highlighted && highlightCentralNodes;
-
-        const nodeColor = isHighlighted
-          ? "#ff9900"
-          : node.color ||
-            (selectedMetric === "PageRank Centrality"
-              ? "orange"
-              : selectedMetric === "Eigenvector Centrality"
-              ? "purple"
-              : selectedMetric === "Closeness Centrality"
-              ? "green"
-              : selectedMetric === "Betweenness Centrality"
-              ? "red"
-              : selectedMetric === "Degree Centrality"
-              ? "#231d81"
-              : "blue");
-
-        ctx.fillStyle = nodeColor;
-        ctx.fill();
-
-        if (node.isHighlightedCommunity) {
-          ctx.save();
-          ctx.shadowColor = "#F5BD20";
-          ctx.shadowBlur = 15;
-          ctx.shadowOffsetX = 0;
-          ctx.shadowOffsetY = 0;
-          ctx.strokeStyle = "#F5BD20";
-          ctx.lineWidth = 2;
-          ctx.stroke();
-          ctx.restore();
-        } else if (isHighlighted) {
-          ctx.strokeStyle = "#ffff00";
-          ctx.lineWidth = 3;
-          ctx.stroke();
-        }
-
-        ctx.font = `${fontSize}px Sans-Serif`;
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.fillStyle = "white";
-        ctx.fillText(node.id, node.x, node.y);
-
-        if (selectedMetric === "Degree Centrality") {
-          ctx.fillStyle = "#231d81";
-          ctx.fillText(`Deg: ${node.degree}`, node.x, node.y + radius + 5);
-        }
-        if (selectedMetric === "Betweenness Centrality") {
-          ctx.fillStyle = "DarkRed";
-          ctx.fillText(
-            `Btw: ${node.betweenness?.toFixed(2) || 0}`,
-            node.x,
-            node.y + radius + 5
-          );
-        }
-        if (selectedMetric === "Closeness Centrality") {
-          ctx.fillStyle = "green";
-          ctx.fillText(
-            `Cls: ${node.closeness?.toFixed(2) || 0}`,
-            node.x,
-            node.y + radius + 5
-          );
-        }
-        if (selectedMetric === "Eigenvector Centrality") {
-          ctx.fillStyle = "purple";
-          ctx.fillText(
-            `Eig: ${node.eigenvector?.toFixed(4) || 0}`,
-            node.x,
-            node.y + radius + 5
-          );
-        }
-        if (selectedMetric === "PageRank Centrality") {
-          ctx.fillStyle = "orange";
-          ctx.fillText(
-            `PR: ${node.pagerank?.toFixed(4) || 0}`,
-            node.x,
-            node.y + radius + 5
-          );
-        }
-
-        ctx.restore();
-      }}
-      onNodeHover={(node) => {
-        if (node && networkWasRestored) {
-          document.body.style.cursor = "pointer";
-        } else {
-          document.body.style.cursor = "default";
-        }
-      }}
-    />
+        }}
+        onNodeHover={(node) => {
+          if (node && networkWasRestored) {
+            document.body.style.cursor = "pointer";
+          } else {
+            document.body.style.cursor = "default";
+          }
+        }}
+      />
     </div>
   );
 };
@@ -292,16 +295,6 @@ NetworkGraph.propTypes = {
   networkWasRestored: PropTypes.bool,
   forceGraphRef: PropTypes.object,
   isDirectedGraph: PropTypes.bool,
-};
-
-NetworkGraph.defaultProps = {
-  filteredNodes: [],
-  filteredLinks: [],
-  highlightCentralNodes: false,
-  showMetrics: true,
-  visualizationSettings: {},
-  networkWasRestored: false,
-  isDirectedGraph: false,
 };
 
 export default NetworkGraph;
