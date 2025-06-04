f456f8df5f878c3ea98a1698bab2bb6a47a3b837
Tslil Aharon <59604062+tslilaharon@users.noreply.github.com>
2025-06-04 13:50:22 +0300

    feat: implement avatar upload, edit, and proper serving from backend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,20 +1,34 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
-import { Bell, Gear } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./Header.css";
 
 const Header = ({ isOpen }) => {
+  const { currentUser } = useSelector((state) => state.user);
+  const baseURL = import.meta.env.VITE_API_URL;
+  const isFullUrl = (url) =>
+    url.startsWith("http://") || url.startsWith("https://");
+
   return (
     <div className={`header-wrapper ${isOpen ? "open" : "closed"}`}>
       <Navbar className="header-navbar">
-        <Container fluid className="d-flex justify-content-end align-items-center">
+        <Container
+          fluid
+          className="d-flex justify-content-end align-items-center"
+        >
           <div className="icon-container">
             <div className="profile-container">
               <Link to="/profile">
                 <Image
-                  src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+                  src={
+                    currentUser?.avatar && currentUser.avatar.trim() !== ""
+                      ? isFullUrl(currentUser.avatar)
+                        ? currentUser.avatar
+                        : `${baseURL}${currentUser.avatar}`
+                      : "https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+                  }
                   roundedCircle
                   alt="Profile"
                   className="profile-image"
